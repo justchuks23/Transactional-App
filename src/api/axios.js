04a84b22bb8a83e944da0a90a61d5d96ab1fa8fd
Justin Chukwuebuka Egwuasi <chukwebukaegwuasi.bincom@gmail.com>
2026-05-01 22:08:01 +0100

    Added additional urls

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://transaction-app-uhk4.onrender.com/',
   headers: {
     'Content-Type': 'application/json',
   },
