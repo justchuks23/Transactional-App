00aa91fcb88b10e29a5c59953e58f79e71facee8
Justin Chukwuebuka Egwuasi <chukwebukaegwuasi.bincom@gmail.com>
2026-06-05 17:21:49 +0100

    added argon_cff

--- app/main.py
+++ app/main.py
@@ -20,7 +20,7 @@ app = FastAPI(
 # CORS setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Replace "*" with frontend URL in production
+    allow_origins=["http://localhost:3000, https://transactional-app-zvo6-git-master-justchuks23s-projects.vercel.app"],  # Replace "*" with frontend URL in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
